0310bda0fbaa1e7a9462055637fb4277bdd5cb87
Phil Kunz <github@philkunz.com>
2018-11-26 23:24:36 +0100

    fix(core): update

--- ts/scalyr.classes.aggregator.ts
+++ ts/scalyr.classes.aggregator.ts
@@ -10,33 +10,38 @@ export class ScalyrAggregator {
   }
 
   public async addLogPackage(logPackage: plugins.smartlogInterfaces.ILogPackage) {
-    const response = await plugins.smartrequest.postJson(this.logUrl, {
-      requestBody: {
-        token: this.scalyrRef.scalyrToken,
-        session: 'test-session',
-        sessionInfo: {
-          serverType: 'frontend',
-          serverId: 'prod-front-2'
-        },
-        events: [
-          {
-            thread: '1',
-            ts: (Date.now() * 1000000).toString(),
-            type: 0,
-            sev: 3,
-            attrs: {
-              message: 'record retrieved',
-              recordId: 39217,
-              latency: 19.4,
-              length: 39207
-            }
+    const requestBody = {
+      token: this.scalyrRef.scalyrToken,
+      session: 'test-session',
+      sessionInfo: {
+        serverType: 'frontend',
+        serverId: 'prod-front-2'
+      },
+      events: [
+        {
+          thread: '1',
+          ts: (Date.now() * 1000000).toString(),
+          type: 0,
+          sev: 3,
+          attrs: {
+            message: 'record retrieved',
+            recordId: 39217,
+            latency: 19.4,
+            length: 39207
           }
-        ],
-        threads: [
-          { id: 1, name: 'request handler thread' },
-          { id: 2, name: 'background processing thread' }
-        ]
-      }
+        }
+      ],
+      threads: [
+        { id: 1, name: 'request handler thread' },
+        { id: 2, name: 'background processing thread' }
+      ]
+    }
+
+    console.log('sending the following request body:')
+    console.log({...requestBody, token: 'not shown for security reasons using spread operator'});
+
+    const response = await plugins.smartrequest.postJson(this.logUrl, {
+      requestBody
     });
     console.log(response.body);
   }
